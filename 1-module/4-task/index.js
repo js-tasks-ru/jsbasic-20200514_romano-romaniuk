37078de3dfe0439e8c0018faf194f2d382f51cf3
Roman Romaniuk <romano.romaniuk@gmail.com>
2020-07-14 16:36:22 +0200

    check-spam function has been created

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,13 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-
+  let newStr = str.toLowerCase();
+  //
+  if (
+    newStr.includes("1xbet")
+    ||newStr.includes("xxx")
+  ) {
+      return true;
+  }
+  else return false;
 }
