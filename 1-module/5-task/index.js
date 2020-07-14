325b4ce41cac42d00d42e826fe15ca110e00d690
Roman Romaniuk <romano.romaniuk@gmail.com>
2020-07-14 17:15:16 +0200

    string-cutting function: done

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,9 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-
+  if (str.length > maxlength) {
+    let newStr = str.slice(0, maxlength - 1) + "…";
+    return newStr;
+  }
+  else return str;
 }
