a28ca9399f7eecb3c049780a809d8ec1909ddbf8
Roman Romaniuk <romano.romaniuk@gmail.com>
2020-07-14 13:04:44 +0200

    factorial function has been created

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @returns {number}
  */
 function factorial(n) {
-
+  if (n == 1 || n == 0) return 1;
+  //
+  let result = n;
+  let k = n - 1;   // k - коэфициент умножения
+  //
+  for (let i = 0 ; i < n - 1; i++){
+      result *=  k; 
+      k-- ;
+    }
+  return result
 }
