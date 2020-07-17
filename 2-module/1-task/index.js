3d82d1a063accb4eb003807d2dedc9cf1ddb1348
Roman Romaniuk <romano.romaniuk@gmail.com>
2020-07-17 20:40:28 +0200

    function sumSalaries: done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-
+  let sum = 0;
+  //
+  for (let key in salaries) {
+    if (!isFinite(salaries[key])) continue;
+    sum += salaries[key];
+  }
+  return sum;
 }
